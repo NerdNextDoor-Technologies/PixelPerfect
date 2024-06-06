d9b97d9a1a6603fd1e5fb27383acd538c8b5030c
Ribhav Jain <ribhav@nerdnextdoor.in>
2024-06-06 22:27:42 +0530

    eslint

--- vue-project/eslint.config.js
+++ vue-project/eslint.config.js
@@ -4,7 +4,7 @@ import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
