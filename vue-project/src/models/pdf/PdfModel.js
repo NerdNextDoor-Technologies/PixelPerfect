bf3bf2a29ce296c65bcfa7ca16a2e1bcdf9e5524
Ribhav Jain <ribhav@nerdnextdoor.in>
2024-06-27 14:25:42 +0530

    to fix variable names

--- vue-project/src/models/pdf/PdfModel.js
+++ vue-project/src/models/pdf/PdfModel.js
@@ -1,21 +1,25 @@
+//FIXME Rename states to something relevant
 const States = {
-  INIT: 'init',
-  SELECTED: 'selected',
-  LOADING: 'loading',
-  TO_BE_DOWNLOADED: 'toBeDownloaded'
+  INIT: 'INIT',
+  SELECTED: 'SELECTED',
+  LOADING: 'LOADING',
+  TO_BE_DOWNLOADED: 'TO_BE_DOWNLOADED'
 };
 
 class PdfData {
   constructor() {
+
+    //fixme variable name
     /** @type {string} */
     this.state = States.INIT;
 
     /** @type {{ filename: string, url: string } | null} */
     this.file = null;
 
-    /** @type {string} */
+    /** @type {URL} */
     this.downloadLink = '';
 
+    //FIXME capitalize
     /** @type {'low' | 'medium' | 'high'} */
     this.compressionLevel = 'low';
   }
