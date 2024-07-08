e87913c714fd6ca9f383668b3f7f0f03e1705925
Ribhav Jain <ribhav@nerdnextdoor.in>
2024-07-08 17:26:34 +0530

    changed the branch (log)rocket

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -9,6 +9,8 @@ import * as Sentry from "@sentry/vue";
 LogRocket.init('5sgire/pixelperfect');
 
 
+
+
 const app = createApp(App);
 
 
@@ -26,6 +28,12 @@ Sentry.init({
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+LogRocket.getSessionURL(sessionURL => {
+    Sentry.configureScope(scope => {
+        scope.setExtra("sessionURL", sessionURL);
+    });
 });
 
 app.use(createPinia());
