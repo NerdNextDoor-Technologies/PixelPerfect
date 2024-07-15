fef58b5a0e04e6b2ae0b3cbfabf5dd73e63838a3
Ribhav Jain <ribhav@nerdnextdoor.in>
2024-07-15 12:17:51 +0530

    variable names of the image helper functions corrected

--- vue-project/src/helpers/ImageHelper.js
+++ vue-project/src/helpers/ImageHelper.js
@@ -1,27 +1,27 @@
 
 //TODO Add Data types to the functions
-//FIXME rename img to imageElement
+//FIXME rename imageElement to imageElement
 //FIXME rename targetWidth to targetWidthInPixels
 //FIXME rename targetHeight to targetHeightInPixels
 //FIXME combine targetHeight and targetWidth into one object targetSizeInPixels
-//FIXME rename canvas to canvasElement
-//FIXME rename ctx to canvasContext
-export function resizeImage(img, imageResolution) {
-  const canvas = document.createElement('canvas');
-  canvas.width = imageResolution.width;
-  canvas.height = imageResolution.height;
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, imageResolution.width, imageResolution.height);
-  return canvas.toDataURL('image/jpeg');
+//FIXME rename canvasElement to canvasElementElement
+//FIXME rename canvasElementContext to canvasElementContext
+export function resizeImage(imageElement, targetimageResolution) {
+  const canvasElement = document.createElement('canvas');
+  canvasElement.width = targetimageResolution.width;
+  canvasElement.height = targetimageResolution.height;
+  const canvasElementContext = canvasElement.getContext('2d');
+  canvasElementContext.drawImage(imageElement, 0, 0, targetimageResolution.width, targetimageResolution.height);
+  return canvasElement.toDataURL('image/jpeg');
 }
 
-export function resizeImage1(img, width, height, quality = 1.0) {
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, width, height);
-  return canvas.toDataURL('image/jpeg', quality);
+export function resizeImage1(imageElement, width, height, quality = 1.0) {
+  const canvasElement = document.createElement('canvas');
+  canvasElement.width = width;
+  canvasElement.height = height;
+  const canvasElementContext = canvasElement.getContext('2d');
+  canvasElementContext.drawImage(imageElement, 0, 0, width, height);
+  return canvasElement.toDataURL('image/jpeg', quality);
 }
 
 
@@ -55,18 +55,8 @@ export function reduceImageToSize(image, targetSizeInBytes) {
     }
   }
 }
-//FIXME Rename targetSize to targetSizeInBytes
-//FIXME Rename image to imageElement
-//FIXME Rename dataURL to resizedImage
-//FIXME Rename quality to qualityFactor
-//FIXME Rename resultSize to resizedImageSize
-// export function resizeImage(image: Image, targetSizeInPixels: ImageResolution) {
-//   // TODO Implement a better algorithm to reduce the image size -
-//   // Calculate the reduction ratio of current image size to target size
-//   // Call resizeImage with the reduction ratio as targetSizeInPixels
-//   // If the resized image size is still greater than targetSizeInBytes, repeat the process
-//   // If the resized image size is less than targetSizeInBytes, return the resized image
-//   // If the resized image is still greater than a minimum target size, start the entire process with a reduced quality factor
+
+
 
 
 
@@ -89,21 +79,21 @@ export function reduceImageToSize(image, targetSizeInBytes) {
 //   // }
 
 
-//   const canvas = document.createElement('canvas');
-//   const ctx = canvas.getContext('2d');
+//   const canvasElement = document.createElement('canvasElement');
+//   const canvasElementContext = canvasElement.getContext('2d');
 
 //   let quality = 0.9;
 //   let resultSize = targetSize + 1; // Initial size for the loop condition
 //   let dataURL = '';
 
-//   canvas.width = image.width;
-//   canvas.height = image.height;
+//   canvasElement.width = image.width;
+//   canvasElement.height = image.height;
 
 //   while (resultSize > targetSize) {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+//     canvasElementContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+//     canvasElementContext.drawImage(image, 0, 0, canvasElement.width, canvasElement.height);
 
-//     dataURL = canvas.toDataURL('image/jpeg', quality);
+//     dataURL = canvasElement.toDataURL('image/jpeg', quality);
 //     resultSize = dataURL.length * (3 / 4);
 
 //     quality -= 0.1;
